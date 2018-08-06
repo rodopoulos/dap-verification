304139c3deb7b7a0243b36dd11032f3c72894c76
Felipe Rodopoulos <felipekss@gmail.com>
2018-08-06 15:56:39 -0300

    corrections and improvements in some DAP transaction rules

--- scripts/DAP_Transaction.thy
+++ scripts/DAP_Transaction.thy
@@ -11,14 +11,14 @@ inductive_set daptrans :: "event list set" where
   Nil: "[] \<in> daptrans"
 
   | DT1: "\<lbrakk> evs1 \<in> daptrans \<rbrakk>
-    \<Longrightarrow> Says A Server \<lbrace>Agent A, Number T\<rbrace> # evs1 \<in> daptrans"
+    \<Longrightarrow> Says A Server \<lbrace> Agent A, Number T \<rbrace> # evs1 \<in> daptrans"
 
   | DT2: "\<lbrakk> evs2 \<in> daptrans;
-          Says A' Server \<lbrace>Agent A, Number T\<rbrace> \<in> set evs2;
+          Says A' Server \<lbrace> Agent A, Number T \<rbrace> \<in> set evs2;
           Nonce r \<notin> used evs2;
           r' = Crypt (shrK K) (Nonce r);
-          h_s = Hash \<lbrace> \<lbrace>Agent A, Number T \<rbrace>, r' \<rbrace> \<rbrakk>
-    \<Longrightarrow> Says Server A \<lbrace> Transaction, r', h_s \<rbrace> # evs2 \<in> daptrans"
+          h_s = Hash \<lbrace> \<lbrace> Agent A, Number T \<rbrace>, r' \<rbrace> \<rbrakk>
+    \<Longrightarrow> Says Server A \<lbrace> \<lbrace> Agent A, Number T \<rbrace>, r', h_s \<rbrace> # evs2 \<in> daptrans"
 
   | DT3: "\<lbrakk> evs3 \<in> daptrans;
             Says A Server Transaction \<in> set evs3;
@@ -26,24 +26,36 @@ inductive_set daptrans :: "event list set" where
     \<Longrightarrow> Inputs A (Smartphone A) \<lbrace> Transaction, r', h_s \<rbrace> # evs3 \<in> daptrans"
 
   | DT4: "\<lbrakk> evs4 \<in> daptrans; 
-            Gets_s (Smartphone A) \<lbrace> Transaction, r', h_s \<rbrace>;  
-            h_u = Hash (Transaction, r'); h_s == h_u \<rbrakk> 
+            Gets_s (Smartphone A) \<lbrace> Transaction, r', h_s \<rbrace> \<in> set evs4;  
+            h_u \<notin> used evs4; (* Hash must be fresh and generated at smartphone *)
+            h_u = Hash \<lbrace> Transaction, r' \<rbrace>;
+            h_s == h_u \<rbrakk> 
     \<Longrightarrow> Outputs (Smartphone A) A Transaction # evs4 \<in> daptrans"
 
   | DT5: "\<lbrakk> evs5 \<in> daptrans;
-            Inputs A (Smartphone A) \<lbrace> Transaction, r', h_s \<rbrace>;
-            Gets_a A Transaction'; Transaction == Transaction' \<rbrakk> 
+            Says A Server Transaction \<in> set evs5;
+            Gets S \<lbrace> Transaction, r', h_u \<rbrace> \<in> set evs5;
+            Inputs A (Smartphone A) \<lbrace> Transaction, r', h_s \<rbrace> \<in> set evs5;
+            Gets_a A Transaction' \<in> set evs5;
+            Transaction == Transaction' \<rbrakk> 
     \<Longrightarrow> Inputs A (Smartphone A) Confirmation # evs4 \<in> daptrans"
 
   | DT6: "\<lbrakk> evs6 \<in> daptrans; 
-            Gets_s (Smartphone A) \<lbrace> Transaction, r', h_s \<rbrace>;
-            Outputs (Smartphone A) A Transaction; 
-            Gets_s (Smartphone A) Confirmation; Confirmation == True;
+            Gets_s (Smartphone A) \<lbrace> Transaction, r', h_s \<rbrace> \<in> set evs6;
+            Outputs (Smartphone A) A Transaction \<in> set evs6; 
+            Gets_s (Smartphone A) Confirmation \<in> set evs6;
             r_u \<notin> used evs6 \<rbrakk> 
    \<Longrightarrow> Outputs (Smartphone A) A r_u # evs6 \<in> daptrans"
 
-   (* TODO *)
-   | DT7: "\<lbrakk> evs7 \<in> daptrans \<rbrakk> \<Longrightarrow> Says A Server r_u # evs7 \<in> daptrans"
+  (* TODO *)
+  | DT7: "\<lbrakk> evs7 \<in> daptrans;
+            Says A Server Transaction \<in> set evs7;
+            Gets S \<lbrace> Transaction, r', h_u \<rbrace> \<in> set evs7; 
+            Inputs A (Smartphone A) \<lbrace> Transaction, r', h_u\<rbrace> \<in> set evs7;
+            Gets_a A Transaction' \<in> set evs7;
+            Inputs A (Smartphone A) Confirmaton \<in> set evs7;
+            Gets_a A r_u \<in> set evs7 \<rbrakk> 
+    \<Longrightarrow> Says A Server r_u # evs7 \<in> daptrans"
 
   | Fake: "\<lbrakk>evsf \<in> daptrans; X \<in> synth(analz(spies evsf))\<rbrakk> \<Longrightarrow> Says Spy B X # evsf \<in> daptrans"
 
