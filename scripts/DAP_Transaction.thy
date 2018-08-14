33249a9aa321f844f21248c47c855e2a33fdbb00
Felipe Rodopoulos <felipekss@gmail.com>
2018-08-14 12:04:15 -0300

    adding new rule for transaction protocol

--- scripts/DAP_Transaction.thy
+++ scripts/DAP_Transaction.thy
@@ -9,6 +9,8 @@ begin
 
 abbreviation
   Confirmation :: msg where "Confirmation \<equiv> (Number 1)"
+abbreviation
+  Success :: msg where "Success \<equiv> (Number 1)"
 
 inductive_set daptrans :: "event list set" where
   Nil: "[] \<in> daptrans"
@@ -29,7 +31,10 @@ inductive_set daptrans :: "event list set" where
             Gets A \<lbrace> Transaction, r', h_s \<rbrace> \<in> set evs3 \<rbrakk> 
     \<Longrightarrow> Inputs A (Smartphone A) \<lbrace> Transaction, r', h_s \<rbrace> # evs3 \<in> daptrans"
 
-  | DT4: "\<lbrakk> evs4 \<in> daptrans; 
+  | DT4: "\<lbrakk> evs4 \<in> daptrans;
+            Says A Server Transaction \<in> set evs4;
+            Says Server A \<lbrace> Transaction, r', h_s \<rbrace> \<in> set evs4;
+            Inputs A (Smartphone A) \<lbrace> Transaction', r', h_s\<rbrace> \<in> set evs4;
             Gets_s (Smartphone A) \<lbrace> Transaction', r', h_s \<rbrace> \<in> set evs4;
             h_u = Hash \<lbrace> Transaction', r' \<rbrace>;
             h_s == h_u \<rbrakk> 
@@ -59,6 +64,13 @@ inductive_set daptrans :: "event list set" where
             Gets_a A r_u \<in> set evs7 \<rbrakk> 
     \<Longrightarrow> Says A Server r_u # evs7 \<in> daptrans"
 
+  | DT8: "\<lbrakk> evs8 \<in> daptrans;
+            Gets Server Transaction \<in> set evs8;
+            Says Server A \<lbrace> Transaction, r', h_u \<rbrace> \<in> set evs8;
+            Gets Server r_u \<in> set evs8; 
+            r_u == r' \<rbrakk>
+    \<Longrightarrow> Says Server A Success # evs8 \<in> daptrans"
+
   | Fake: "\<lbrakk> evsf \<in> daptrans; X \<in> synth(analz(knows Spy evsf)) \<rbrakk> \<Longrightarrow> Says Spy B X # evsf \<in> daptrans"
     
   | Rcpt: "\<lbrakk> evsr \<in> daptrans; Says A B X \<in> set evsr \<rbrakk> \<Longrightarrow> Gets B X # evsr \<in> daptrans"
@@ -73,7 +85,8 @@ inductive_set daptrans :: "event list set" where
 lemma DT3_happens:
   "\<exists> T r h. \<exists>evs \<in> daptrans. Inputs A (Smartphone A) \<lbrace> T, r, h \<rbrace> \<in> set evs"
 apply (intro exI bexI)
-apply (rule_tac [2] daptrans.Nil [THEN daptrans.DT1, THEN daptrans.Rcpt,
+apply (rule_tac [2] daptrans.Nil 
+        [THEN daptrans.DT1, THEN daptrans.Rcpt,
         THEN daptrans.DT2, THEN daptrans.Rcpt,
         THEN daptrans.DT3])
 apply (possibility, auto)
@@ -82,15 +95,39 @@ done
 lemma DT4_happens:
   "\<exists> T. \<exists>evs \<in> daptrans. Outputs (Smartphone A) A T \<in> set evs"
 apply (intro exI bexI)
-apply (rule_tac [2] daptrans.Nil [THEN daptrans.DT1, THEN daptrans.Rcpt,
+apply (rule_tac [2] daptrans.Nil 
+        [THEN daptrans.DT1, THEN daptrans.Rcpt,
         THEN daptrans.DT2, THEN daptrans.Rcpt,
         THEN daptrans.DT3, THEN daptrans.Rcpt_s,
         THEN daptrans.DT4])
 apply (possibility, auto)
 done    
-    
+
+lemma DT5_happens:
+  "\<exists> C. \<exists>evs \<in> daptrans. Inputs A (Smartphone A) C \<in> set evs"
+apply (intro exI bexI)
+(*apply (rule_tac [2] daptrans.Nil
+        [THEN daptrans.DT1, THEN daptrans.Rcpt,
+        THEN daptrans.DT2, THEN daptrans.Rcpt,
+        THEN daptrans.DT3, THEN daptrans.Rcpt_s,
+        THEN daptrans.DT4, THEN daptrans.Rcpt_a,
+        THEN daptrans.DT5])*)
+apply (rule_tac [2] daptrans.DT5)
+apply (rule_tac [2] daptrans.Rcpt_a)
+apply (rule_tac [2] daptrans.DT4)
+apply (rule_tac [2] daptrans.Rcpt_s)
+apply (rule_tac [2] daptrans.DT3)
+apply (rule_tac [2] daptrans.Rcpt)
+apply (rule_tac [2] daptrans.DT2)
+apply (rule_tac [2] daptrans.Rcpt)
+apply (rule_tac [2] daptrans.DT1)
+apply (rule_tac [2] daptrans.Nil)
+apply (possibility)
+apply (auto)
+done
+
 lemma Protocol_terminates :
-  "\<exists>r. \<exists>evs \<in> daptrans. Says A Server (Nonce r) \<in> set evs"
+  "\<exists>Success. \<exists>evs \<in> daptrans. Says Server A Success \<in> set evs"
 apply (intro exI bexI)
 apply (rule_tac [2] daptrans.Nil [THEN daptrans.DT1, THEN daptrans.Rcpt,
         THEN daptrans.DT2, THEN daptrans.Rcpt,
@@ -98,7 +135,8 @@ apply (rule_tac [2] daptrans.Nil [THEN daptrans.DT1, THEN daptrans.Rcpt,
         THEN daptrans.DT4, THEN daptrans.Rcpt_a,
         THEN daptrans.DT5, THEN daptrans.Rcpt_s,
         THEN daptrans.DT6, THEN daptrans.Rcpt_a,
-        THEN daptrans.DT7])
+        THEN daptrans.DT7, THEN daptrans.Rcpt,
+        THEN daptrans.DT8])
 apply (possibility, auto)
 oops
 
